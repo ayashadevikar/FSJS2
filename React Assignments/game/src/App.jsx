98d19ded3b2becd4c928d4d20a458f04405377f7
ayashadevikar <ayashadevikar8@gmail.com>
2023-01-17 18:41:20 +0530

    Add some changes for 17-01-2023 file.

--- React Assignments/game/src/App.jsx
+++ React Assignments/game/src/App.jsx
@@ -1,34 +1,30 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+
+
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
+
+  const [userChoice, setuserChoice] = useState("Rock,Paper,Scissor")
+  const [computerChoice, setcomputerChoice] = useState("Rock,Paper,Scissor")
+
+
+  //  if(userChoice === computerChoice){
+  //        alert("Game Draw")
+  //        return;
+  //  }
+
+  const Choices = Math.floor(Math.random()*3)
+
 
   return (
-    <div className="App">
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://reactjs.org" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </div>
+     <>
+        <h1>Rock Scissor Paper Game</h1>
+        <button>Rock</button>
+        <button>Scissor</button>
+        <button>Paper</button>
+        
+      </>
   )
 }
 
