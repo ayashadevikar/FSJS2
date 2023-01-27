769748a1cfdf1bb921f63940ceac701e62aa17a0
ayashadevikar <ayashadevikar8@gmail.com>
2023-01-27 13:23:59 +0530

    css and js practice folder added

---
+++ Practice Work/JS/arraymethods.js
@@ -0,0 +1,23 @@
+// Array In Depth and For Loop
+
+// Array Methods
+ let names = ['Alex', 'Harry', 'Daniel', 'Scott', 'John'];
+// console.log(names);
+// console.log(names.length); // Length of Array
+
+// console.log(names[3]);
+// console.log(names[names.length -1]);
+
+// names[3] = 'Joe';
+// console.log(names);
+
+// let names = new Array('Name 1', 'Name 2');  // A way to Declare a Array // Not Recommended
+// console.log(names);
+
+//Push => Inserting New Values inside the Array at the end
+// names.push('Max');
+// console.log(names);
+
+// Slice
+// console.log(names.slice(2,3)); // [ 'Daniel' ]
+// console.log(names.slice(2,4));  // [ 'Daniel', 'Scott' ]
