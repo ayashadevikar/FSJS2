ad475a501ab8040ceff0b4f5fdc9b5539ff660c5
ayashadevikar <ayashadevikar8@gmail.com>
2023-02-01 23:24:14 +0530

    condition.js file added

---
+++ Practice Work/JS/condition.js
@@ -0,0 +1,142 @@
+// conditions
+
+// if else
+// switch case
+// ternary operator / ternary conditions
+
+// Rating mini App
+
+// let rating = 2;
+
+// if(rating == 5){
+//     console.log('5 Stars');
+// } else if (rating == 4) {
+//     console.log('4 Stars');
+// } else if (rating == 3) {
+//     console.log('3 Stars');
+// } else {
+//     console.log('i am else');
+// }
+
+// Switch Case
+
+// let user = 'Admin';
+
+// switch (user) {
+//     case 'Admin':
+//           console.log('Redirect Him/Her to the Admin Page');
+//           break;
+//     case 'Mentor':
+//         console.log('Redirect Him/Her to the Mentor Dashboard');
+//         break;
+
+//         default:
+//             console.log('I am Simple User');
+
+// }
+
+// ternary operator / ternary condition
+
+// condition?True:False
+
+// let raining = false;
+
+// raining ? console.log('It is Raining') : console.log('It is Not Raining');
+
+// let userlogin = true;
+
+// userlogin ? console.log('logout') : console.log('login');
+
+// array
+
+// let name = [
+//     'Ayasha','Shubham','Ankita','Maithali','Shahana','Akshay','Mahesh',
+// ];
+
+// console.log(name);
+// console.log(name[3]);
+
+// object
+
+// let bacnkaccount = {
+//     firstName: 'Nabeela',
+//     lastName: 'Robert',
+//     mobileno: 4537980004,
+//     balance: 1000,
+// };
+
+// console.log(bacnkaccount);
+// console.log(bacnkaccount.balance);
+
+// date and math
+
+// const now = new Date();
+// console.log(now.toString());
+
+// const ayasha = new Date();
+// console.log(ayasha);
+
+
+// console.log(now.getFullYear());
+// console.log(now.getMonth());
+// console.log(now.getDay());
+
+// console.log(now.getTime());
+
+// console.log(now.getHours());
+// console.log(now.getMinutes());
+// console.log(now.getSeconds());
+
+// math
+
+// const PI = Math.PI;
+// console.log(PI);
+
+// console.log(Math.round(PI));
+// console.log(Math.round(9.4));
+// console.log(Math.floor(9.5));
+// console.log(Math.ceil(9.5));
+
+
+// min and max
+
+// console.log(Math.min(10, 1, 2, 3, 5));
+// console.log(Math.max(10, 1, 2, 3, 5));
+
+// random
+
+// console.log(Math.round(Math.random() * 50)); 
+
+// console.log(Math.pow(3,2));
+
+// console.log(Math.log(2));
+
+// console.log(Math.sin(90));
+
+// loops
+
+// do while loop
+
+// let i = 6;
+// do{
+//     console.log('Value of i is:', i);
+//     i++;
+// } while (i <=5 );
+
+
+// let j = 6;
+// while(j <=5) {
+//     console.log('Value of J is', j);
+//     j++;
+// }
+
+// for
+
+// for (let i = 0; i<=5; i++) {
+//     console.log(`${i} * ${i} = ${i * i}`);
+// }
+
+
+
+
+
