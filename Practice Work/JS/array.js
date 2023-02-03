1c4dd0642158d01bce0bf98900382905068f0d13
ayashadevikar <ayashadevikar8@gmail.com>
2023-02-03 23:47:38 +0530

    object file added

---
+++ Practice Work/JS/array.js
@@ -0,0 +1,211 @@
+// let cars = ['Volvo', 'BMW', 'Swift', 'Baleno'];
+// cars.push('Duster');
+// console.log(cars);
+
+// let names = ['Hitesh Sir', 'Anurag', 'Surya', 'Anirudh', 'Bipul'];
+// console.log(names.slice( 4));
+
+// const array1 = ['a', 'b', 'c'];
+// const array2 = ['d', 'e', 'f'];
+// const array3 = array1.concat(array2);
+
+// console.log(array3);
+
+// let array= [1, 2, 3, 4];
+
+// console.log(array.fill(0,1,2));
+
+// let num = [1, 2, 3, 'Anurag', 4, 5, 6, 7, 8, 'Anurag', 'Anurag'];
+// let num1 = 'Anurag';
+//  console.log(Array.isArray(num1));
+// console.log(Array.isArray(num));
+
+// let array = [1, 2, 3, 4];
+// let names = array.join(' 5 ');
+// console.log(names);
+
+// let sort = [5, 3, 1, 4, 2];
+// console.log(sort);
+
+// let fruit = ['Apple', 'Bada Apple', 'Chota Apple', 'Double Apple'];
+
+// fruit.unshift('Apple', 'Grapes');
+// console.log(fruit);
+
+// let name = 'Anurag';
+// let array1 = name.split('');
+// console.log(array1);
+
+// let names = ['sam', 'max'];[5, 4, 3, 2, 1]
+
+
+// let array9 = [1, 2, 3, 4, 5];
+
+// console.log(array9.reverse());
+// console.log(names);
+
+// [1, 2, 3, 4, 5]
+
+
+
+// let array8 = [8, 9, 10, 11, 12];
+
+// console.log(array8.pop());
+
+// console.log(array8);
+
+// [8, 9, 10, 11]
+
+
+// const fruits = ['Apple', 'Banana', 'Grapes', 'Oranges'];
+
+// console.log(fruits.length);
+
+
+
+// 4
+
+// let cars = ['Volvo', 'BMW', 'Swift', 'Baleno'];
+
+// cars.push('Duster');
+
+// console.log(cars);
+
+// [ 'Volvo', 'BMW', 'Swift', 'Baleno', 'Duster' ]
+
+// const animals = ['ant', 'bison', 'camel', 'duck', 'elephant'];
+
+// console.log(animals.slice(2, 4));
+
+// ['camel', 'duck']
+
+// const months = ['Jan', 'March', 'April', 'June'];
+
+// months.splice(1, 0, 'Feb');
+
+// console.log(months);
+
+// ['Jan', 'Feb', 'March', 'April', 'June']
+
+
+// const array1 = ['a', 'b', 'c'];
+
+// const array2 = ['d', 'e', 'f'];
+
+// const array3 = array1.concat(array2);
+
+// console.log(array3);
+
+// [ 'a', 'b', 'c', 'd', 'e', 'f' ]
+
+
+
+
+// let array4 = [1, 2, 3, 4];
+
+// console.log(array4);
+
+// [1, 0, 3, 4]
+
+
+
+// let array1 = ['cat', 'dog', 'cow'];
+
+// console.log(array1.includes('cat'));
+
+// true
+
+
+
+// let array2 = ['Alex', 1, 2, 3];
+
+// console.log(indexOf('Alex'));
+
+// 0
+
+
+
+// let array5 = [4, 5, 6, 7];
+
+// console.log(Array.isArray(5));
+
+// true
+
+
+// let array6 = [1, 2, 3, 4];
+
+// let names = array6.join(' 5 ');
+
+// console.log(var);
+
+// 1 5 2 5 3 5 4
+
+
+// const animals = ['Dodo', 'Tiger', 'Penguin', 'Dodo'];
+
+// console.log(animals.lastIndexOf('Dodo'));
+
+// 3
+
+
+
+// const array1 = [1, 4, 9, 16];
+
+// const map1 = array1.map(x => x * 2);
+
+// console.log(map1);
+
+// [2, 8, 18, 32]
+
+// [1, 2, 3, 4]
+
+
+// let fruit = ['Grapes', 'Banana', 'Apple', 'Oranges'];
+
+// fruit.unshift('Mango', 'Pineapple');
+
+// console.log(fruit);
+
+
+
+// 'Mango',
+
+// 'Pineapple',
+
+// 'Grapes',
+
+// 'Banana',
+
+// 'Apple',
+
+// 'Oranges'
+
+
+// let name = ['Matrix'];
+
+// let array1 = name.split('');
+
+// console.log(array1);
+
+// ['M', 'a', 't', 'r', 'i', 'x']
+
+
+
+
+// [1, 2, 3, 4]
+
+// let maths = [5, 1, 2, 3, 4];
+
+// console.log(maths.shift());
+
+// let sort = [5, 3, 1, 4, 2];
+
+// console.log(sort);
+
+// ['John', 'Max', 'Sam']
+// let names = ['John', 'Max', 'Sam'];
+
+// console.log(names);
+
+let maths = [1, 4, 9, 16, 25];
+console.log(maths.map(Math.sqrt));
