77e4ba55b99f89ea16d9cf5d9f55c4d0ae94fc22
ayashadevikar <ayashadevikar8@gmail.com>
2023-02-06 13:27:27 +0530

    Add some changes for 06-02-2023 file.

--- React Assignments/todo/src/App.jsx
+++ React Assignments/todo/src/App.jsx
@@ -3,7 +3,9 @@ import { useState } from 'react'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
+ 
+  count [add, setAdd] = useState([])
+  count [name, setName] = useState("")
 
   return (
     <>
