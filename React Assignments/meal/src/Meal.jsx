1e1076234a88eacb2b879f0e6d33d5c5128914be
ayashadevikar <ayashadevikar8@gmail.com>
2023-02-10 17:23:06 +0530

    Add some changes for 10-02-2023 file.

--- React Assignments/meal/src/Meal.jsx
+++ React Assignments/meal/src/Meal.jsx
@@ -4,8 +4,17 @@ function Meal({text}){
    return (
 
        <>
+        
+        <div>
+
+       
+        
+            
+        
          <h1>{text.strMeal}</h1>
          <img src={text.strMealThumb} alt="" />
+
+         </div>
        </>
    )
 }
