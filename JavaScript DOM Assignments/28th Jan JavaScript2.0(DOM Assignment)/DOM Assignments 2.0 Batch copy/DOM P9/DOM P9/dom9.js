a21fc5e6b9a8e46d06b9919b897751b6a99394a2
ayashadevikar <ayashadevikar8@gmail.com>
2023-04-06 10:07:28 +0530

    some changes

--- JavaScript DOM Assignments/28th Jan JavaScript2.0(DOM Assignment)/DOM Assignments 2.0 Batch copy/DOM P9/DOM P9/dom9.js
+++
@@ -1,9 +0,0 @@
-document.querySelector(".caption .title").style.color = "#dc143c";
-
-let test = document.querySelector(".add-to-cart");
-test.addEventListener("mouseover",() => {  
-test.style.backgroundColor = "#da123a";
-})
-test.addEventListener("mouseout",() => {  
-test.style.backgroundColor = "hsl(158, 36%, 37%)";
-})
