78994a8adddbaf2edf1af68a50d3093b9e78994a
ayashadevikar <ayashadevikar8@gmail.com>
2023-04-30 14:14:23 +0530

    Add some changes for 30-04-2023 file.

--- Practice Work/BackEnd/socialapp/index.js
+++ Practice Work/BackEnd/socialapp/index.js
@@ -13,6 +13,10 @@ app.get('/', (req, res)=>{
     res.send('Hello World')
 })
 
+app.get('/insta', (req, res)=>{
+    res.status(200).send("Hello")
+})
+
 // Listen
 app.listen(PORT, ()=>{
     console.log("Server Running");
